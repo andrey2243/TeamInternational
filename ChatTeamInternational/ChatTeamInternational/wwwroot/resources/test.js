ecb62952a64570c03a8ce69e3fcb1fb5faa35b1b
andrey22 <bandreus13@gmail.com>
2019-09-20 18:51:09 +0300

    add vue_js client

---
+++ ChatTeamInternational/ChatTeamInternational/wwwroot/resources/test.js
@@ -0,0 +1,10 @@
+﻿import Vue from 'vue';
+import Axios from 'axios';
+import BaseLayout from './components/layout/BaseLayout.vue';
+
+window.axios = Axios;
+
+Vue.component("base-layout", BaseLayout);
+new Vue({
+    el: "#App"
+});
